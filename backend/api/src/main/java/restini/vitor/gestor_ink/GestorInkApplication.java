24211521359335d8789707ea2fcf8b3c92a96871
vrestini <vitor.restini@sptech.school>
2025-12-30 18:47:38 -0300

    feat: api backend em java springboot

---
+++ backend/api/src/main/java/restini/vitor/gestor_ink/GestorInkApplication.java
@@ -0,0 +1,13 @@
+package restini.vitor.gestor_ink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorInkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorInkApplication.class, args);
+	}
+
+}
---
+++ backend/api/src/test/java/restini/vitor/gestor_ink/GestorInkApplicationTests.java
@@ -0,0 +1,13 @@
+package restini.vitor.gestor_ink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorInkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
