646905f79e528e9a20945c0f8b21e712c59ed8a7
vrestini <vitor.restini@sptech.school>
2026-01-02 10:30:53 -0300

    fix: correcao de dependencias.

--- backend/api/gestorInk/src/main/java/restini/vitor/gestorInk/GestorInkApplication.java
+++ backend/api/gestorInk/src/main/java/restini/vitor/gestorInk/GestorInkApplication.java
@@ -1,4 +1,4 @@
-package restini.vitor.gestor_ink;
+package restini.vitor.gestorInk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/api/gestorInk/src/test/java/restini/vitor/gestorInk/GestorInkApplicationTests.java
+++ backend/api/gestorInk/src/test/java/restini/vitor/gestorInk/GestorInkApplicationTests.java
@@ -1,4 +1,4 @@
-package restini.vitor.gestor_ink;
+package restini.vitor.gestorInk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
