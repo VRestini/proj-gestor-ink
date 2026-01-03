730389498c351ffeae9a0a9e05f76b61fdc071b8
vrestini <vitor.restini@sptech.school>
2026-01-03 19:31:41 -0300

    feat: adicao de novos atributos e padronizacao dos nomes AS IS.

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,21 +1,25 @@
 CREATE TABLE IF NOT EXISTS tbl_categoria(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50);
+	nome VARCHAR(50) NOT NULL UNIQUE;
 );
 CREATE TABLE IF NOT EXISTS tbl_produto(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR (60) NOT NULL,
-	descricao VARCHAR(255);
-	validade TINYINT NOT NULL,
-	fkCategoria INT,
+	descricao VARCHAR(255),
+	se_ativo TINYINT NOT NULL,
+	se_validade TINYINT NOT NULL,
+	dt_criacao DATE NOT NULL,
+	fk_categoria INT,
 	FOREIGN KEY (fkCategoria) REFERENCES tbl_categoria(id);
 );
 CREATE TABLE IF NOT EXISTS tbl_item(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	dtEntrada DATE NOT NULL,
-	dtSaida DATE,
-	dtValidade DATE,
+	dt_entrada DATE NOT NULL,
+	dt_saida DATE,
+	dt_validade DATE,
 	valor DECIMAL(6,2),
-	fkProduto INT,
+	se_usado TINYINT NOT NULL,
+	fk_produto INT,
 	FOREIGN KEY (fkProduto) REFERENCES tbl_produto(id);	
 );
+
